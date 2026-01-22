6283fc98ef550140f9a9cbd024c49acda92fd728
Roshni Reghunath <reghunathroshni@gmaill.com>
2026-01-22 16:21:04 +0530

    1st commit

---
+++ hello.java
@@ -0,0 +1,28 @@
+class hello {
+public static void main(String a[]){
+
+    System.out.println("Git testing");
+int i=0;
+    while(i<=5){
+
+System.out.println("Helloo");
+i++;
+    }
+int x = 4;
+int y = 16;
+int z = 10;
+
+if(x>y)
+{
+System.out.println(x);
+}
+else if(y>z){
+    System.out.println(y);
+}
+else{
+    System.out.println(z);
+}
+
+
+}
+}
